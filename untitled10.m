f14f44a4f2755ec003e4dce47db2f0a9202f6ec1
Shibam9938 <140053518+Shibam9938@users.noreply.github.com>
2023-08-28 12:42:17 +0530

    Add files via upload

--- untitled10.m
+++ untitled10.m
@@ -1,8 +1,24 @@
-a = randi([0,10],1,1)
-if a>5;
-    c= a*5;
-    fprintf('product =%d \n',c);
-else a<5;
-    d=a/5;
-    fprintf('division = %d \n',d);
+for n=1:10
+    disp(n)
+end
+close all
+clear all
+for n=2:2:100
+    disp(n)
+end
+close all 
+clear all
+i=0
+for n = 1:3
+    x=2*n^2+n+1
+    disp(x)
+    i=i+x
+    disp(i)
+end
+close all
+clear all
+for n= 1:5
+    if n>3
+        disp(n)
+    end
 end
